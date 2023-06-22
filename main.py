09952ec783be0d30bc888995a246116e157ee4c6
IAS <ivanov7as@gmail.com>
2023-06-22 11:54:34 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from random import randint
+
+num = randint(0, 1000)
+tries = 10
+for i in range(tries+1):
+    if i == tries:
+        print("Попыток больше нет")
+        break
+    x = int(input("Введите число "))
+    if x == num:
+        print("Верно")
+        break
+    if x < num:
+        print("Верное число больше")
+    if x > num:
+        print("Верное число меньше")
